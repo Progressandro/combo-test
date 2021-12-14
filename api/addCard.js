fedd0b16f10d2818249c3048be32ee2555375d12
Alejandro F <progressandro@gmail.com>
2021-12-14 19:44:42 -0400

    CORS.

--- api/addCard.js
+++ api/addCard.js
@@ -2,6 +2,16 @@ import initSDK from "../firebase/init"
 import firebaseAdmin from "firebase-admin"
 
 export default async function addCard(req, res) {
+  res.setHeader("Access-Control-Allow-Credentials", true)
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  )
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  )
   try {
     const ref = await firebaseAdmin
       .firestore()
