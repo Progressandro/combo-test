53979cf7a060bcd394628d9210e3a78cd0d5ccf6
Alejandro F <progressandro@gmail.com>
2021-12-14 19:22:21 -0400

    Fix.

--- api/getCards.js
+++ api/getCards.js
@@ -4,7 +4,7 @@ export default async function getCards(req, res) {
   try {
     initSDK()
     const cards = await firebaseAdmin.firestore().collection("cards").get()
-    res.json(cards)
+    res.json(cards.docs)
   } catch (e) {
     console.error(e)
     res.status(500).json({ message: "F" })
